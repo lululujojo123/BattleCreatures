7aaf10d552c0671410d24d0ac6f8557fd122b85d
Bednarik Lara <lb150@hdm-stuttgart.de>
2020-12-31 12:26:19 +0100

    [Enhancement] Adding ProfileActivity

---
+++ Android/BattleCreatures/app/src/main/java/org/battlecreatures/activities/ProfileActivity.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020 lululujojo123
+ *
+ *  ProfileActivity.kt
+ *
+ *  created by: Lara B.
+ *  last edit \ by: 2020/12/30 \ Lara B.
+ */
+
+package org.battlecreatures.activities
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import org.battlecreatures.R
+
+/**
+ * The profile activity showing the user's profile
+ */
+class ProfileActivity : AppCompatActivity() {
+    companion object{
+        /**
+         * Static constant with the class name for logging purposes
+         */
+        private const val TAG: String = "ProfileActivity"
+    }
+
+    /**
+     * Android related onCreate method
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Super classes onCreate method
+        super.onCreate(savedInstanceState)
+
+        // Initializing the context by using the activity_profile.xml
+        setContentView(R.layout.activity_profile)
+    }
+}
