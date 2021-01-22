5ca28fb84fab165040dcc32130264ca64997da0d
Bednarik Lara <lb150@hdm-stuttgart.de>
2021-01-22 21:21:41 +0100

    [Enhancement] ProfileActivity Progress Bar and Screen Animation

--- Android/BattleCreatures/app/src/main/java/org/battlecreatures/activities/ProfileActivity.kt
+++ Android/BattleCreatures/app/src/main/java/org/battlecreatures/activities/ProfileActivity.kt
@@ -11,10 +11,12 @@ package org.battlecreatures.activities
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import org.battlecreatures.R
+import org.battlecreatures.animations.ProgressBarAnimation
 import org.battlecreatures.logics.database.BCDatabase
 import org.battlecreatures.logics.entities.Player
 
@@ -56,8 +58,11 @@ class ProfileActivity : AppCompatActivity() {
         playerNameTextView.text = ownProfile.name
         currentLevelTextView.text = ownProfile.getLevel().toString()
         nextLevelTextView.text = (ownProfile.getLevel() + 1).toString()
-        expProgressBar.progress = ownProfile.getExpProgress().toInt()
         neededExpTextView.text = ownProfile.getExpForNextLevel().toString()
+        expProgressBar.progress = 0
+
+        //animate the screen elements
+        animateScreen()
 
         changeNameButton.setOnClickListener {
             changeNameButton.isClickable = false
@@ -84,16 +89,58 @@ class ProfileActivity : AppCompatActivity() {
                 changeNameButton.isClickable = true
             } else {
                 //name is too short or too long
-                Toast.makeText(this, getString(R.string.wrong_name_length), Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.wrong_name_length), Toast.LENGTH_LONG).show()
                 confirmNameButton.isClickable = true
             }
 
         }
 
-
         backButton.setOnClickListener {
-            onBackPressed()
+            onBackPressed() //back to home activity
         }
 
+    }
+
+    /**
+     * Private method preparing and starting the animation for the screen initialization
+     */
+    private fun animateScreen() {
+        val backButton : ImageView = findViewById(R.id.backButton)
+        val changeNameButton : ImageView = findViewById(R.id.changeNameButton)
+        val playerNameTextView : TextView = findViewById(R.id.playerNameTextView)
+        val currentLevelTextView : TextView = findViewById(R.id.currentLevel)
+        val nextLevelTextView : TextView = findViewById(R.id.nextLevel)
+        val expProgressBar : ProgressBar = findViewById(R.id.expProgressBar)
+        val neededExpTextView : TextView = findViewById(R.id.neededExp)
+
+        val bcDatabase = BCDatabase.getMainThreadBCDatabase(applicationContext)
+        val playerDAO = bcDatabase.playerDao()
+        var ownProfile = playerDAO.getOwnProfile()
+
+        playerNameTextView.text = ownProfile.name
+        currentLevelTextView.text = ownProfile.getLevel().toString()
+        nextLevelTextView.text = (ownProfile.getLevel() + 1).toString()
+        neededExpTextView.text = ownProfile.getExpForNextLevel().toString()
+        expProgressBar.progress = 0
+
+        // Prepare the screen animation
+        val screenAnimation: Animation = AnimationUtils.loadAnimation(this,
+            R.anim.shield_animation
+        )
+        screenAnimation.duration = 1000
+        //backButtonAnimation.startOffset = 950
+
+        backButton.startAnimation(screenAnimation)
+        changeNameButton.startAnimation(screenAnimation)
+        playerNameTextView.startAnimation(screenAnimation)
+        currentLevelTextView.startAnimation(screenAnimation)
+        nextLevelTextView.startAnimation(screenAnimation)
+        expProgressBar.startAnimation(screenAnimation)
+
+        // Prepare and start the progress bar animation
+        expProgressBar.animation = ProgressBarAnimation(expProgressBar, expProgressBar.progress.toFloat(), ownProfile.getExpProgress().toFloat())
+        expProgressBar.animation.duration = 1000
+        expProgressBar.animation.setInterpolator(this, android.R.interpolator.decelerate_cubic)
+        expProgressBar.animate()
     }
 }
