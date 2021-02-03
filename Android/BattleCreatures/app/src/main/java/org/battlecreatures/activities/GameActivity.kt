500d70ebd9cf6562cb1efb9a3cfd4872ccc214e1
Bednarik Lara <lb150@hdm-stuttgart.de>
2021-02-03 14:01:35 +0100

    [Enhancement] Adding GameActivity

---
+++ Android/BattleCreatures/app/src/main/java/org/battlecreatures/activities/GameActivity.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021 lululujojo123
+ *
+ * GameActivity.kt
+ *
+ * created by: Lara B.
+ * last edit \ by: 2021/02/03 \ Lara B.
+ */
+
+package org.battlecreatures.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.battlecreatures.R
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
